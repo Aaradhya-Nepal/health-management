2a0a0e0ed0f53c9456c77b9f9447c07361a0c298
aaradhya <aaradhyanepal1@gmail.com>
2024-10-24 00:28:57 +0545

    feature: build patient form

---
+++ components/app/patients/forms/PatientForm.tsx
@@ -0,0 +1,70 @@
+"use client"
+
+import {zodResolver} from "@hookform/resolvers/zod"
+import {useForm} from "react-hook-form"
+import {z} from "zod"
+import {Button} from "@/components/ui/button"
+import {Form,} from "@/components/ui/form"
+import FormField from "@/components/app/form-components/CustomFormField";
+import {FormFieldTypes} from "@/enums/FormFieldTypes";
+import icons from "@/constants/icons";
+
+const formSchema = z.object({
+    username: z.string().min(2, {
+        message: "Username must be at least 2 characters.",
+    }),
+})
+
+const PatientForm = () => {
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            username: "",
+        },
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log(values)
+    }
+
+    return (
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 flex-1">
+                <section className="mb-12 space-y-4">
+                    <h1 className="header">Hi there 👋</h1>
+                    <p className="text-dark-700">Schedule your first appointment</p>
+                </section>
+                <FormField
+                    control={form.control}
+                    fieldType={FormFieldTypes.INPUT}
+                    name="name"
+                    label="Full name"
+                    placeholder="John Doe"
+                    iconSrc={icons.user.icon}
+                    iconAlt={icons.user.alt}
+                />
+                <FormField
+                    control={form.control}
+                    fieldType={FormFieldTypes.INPUT}
+                    name="email"
+                    label="Email"
+                    placeholder="john@example-mail.com"
+                    iconSrc={icons.email.icon}
+                    iconAlt={icons.email.alt}
+                />
+                <FormField
+                    control={form.control}
+                    fieldType={FormFieldTypes.PHONE_INPUT}
+                    name="phone"
+                    label="Phone number"
+                    placeholder="(555) 123-4567"
+                    iconSrc={icons.phone.icon}
+                    iconAlt={icons.phone.alt}
+                />
+                <Button type="submit">Submit</Button>
+            </form>
+        </Form>
+    );
+}
+
+export default PatientForm;
