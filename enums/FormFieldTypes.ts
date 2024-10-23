be0ac94dad270acab3c136f425ac97cd4439181c
aaradhya <aaradhyanepal1@gmail.com>
2024-10-24 00:28:14 +0545

    feature: add enums

---
+++ enums/FormFieldTypes.ts
@@ -0,0 +1,9 @@
+export enum FormFieldTypes {
+    INPUT = "input",
+    TEXTAREA = "textarea",
+    PHONE_INPUT = "phoneInput",
+    CHECKBOX = "checkbox",
+    DATE_PICKER = "datePicker",
+    SELECT = "select",
+    SKELETON = "skeleton",
+}
