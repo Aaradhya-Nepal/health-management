59029843a8b77e36a13fd9a4c6da4fa6e73bbbae
aaradhya <aaradhyanepal1@gmail.com>
2024-10-26 17:51:51 +0545

    feature: add new source for image

--- constants/source.ts
+++ constants/source.ts
@@ -1,6 +1,7 @@
 const source = {
     patient: "/assets/icons/logo-full.svg",
     onboardingImage: "/assets/images/onboarding-img.png",
+    registerImage: "/assets/images/register-img.png",
     loader: "/assets/icons/loader.svg",
 }
 
