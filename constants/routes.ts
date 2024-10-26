a9e84277405c6cb05517680f1d7049a3ffdd13af
aaradhya <aaradhyanepal1@gmail.com>
2024-10-26 22:33:17 +0545

    feature: add sources and routes constants

--- constants/routes.ts
+++ constants/routes.ts
@@ -2,7 +2,9 @@ const routes = {
     home: '/',
     admin: '/?admin=true',
     login: '/login',
-    register: `/patients/:userId/register`,
+    register: '/patients/:userId/register',
+    newAppointment: '/patients/:userId/new-appointment',
+    newAppointmentSuccess: '/patients/:userId/new-appointment/success?appointmentId=:appointmentId',
 }
 
 export default routes;
--- constants/source.ts
+++ constants/source.ts
@@ -2,6 +2,7 @@ const source = {
     patient: "/assets/icons/logo-full.svg",
     onboardingImage: "/assets/images/onboarding-img.png",
     registerImage: "/assets/images/register-img.png",
+    appointmentImage: "/assets/images/appointment-img.png",
     loader: "/assets/icons/loader.svg",
 }
 
