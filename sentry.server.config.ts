0a9c57b1f5d8ec1c8d48c09be22a3cf078c378d8
aaradhya <aaradhyanepal1@gmail.com>
2024-10-27 18:59:21 +0545

    feature: add sentry to secure application whenever error occurs

---
+++ sentry.server.config.ts
@@ -0,0 +1,15 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://685ff4205d5cff060bad8a45ef0395f0@o4508194708783104.ingest.de.sentry.io/4508194712846416",
+
+  // Define how likely traces are sampled. Adjust this value in production, or use tracesSampler for greater control.
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+});
